89e308b44868b655104c6cf912adf0aad85b0bec
nortonlyr <nortonlyr@gmail.com>
2020-10-09 22:35:05 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import pickle
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
 
 # pandas read csv
-names = ['label', 'content']
-dataframe = pd.read_csv('shuffled-full-set-hashed.csv', names=names)
+df = pd.read_csv('shuffled-full-set-hashed.csv', names=['label', 'content'])
 
 
-print(dataframe.label.size)
+#print(df.label.size)
+#df.label.value_counts().plot(kind='bar', figsize=(16,9))
